e2ee8f279c8d33bede395866b8f43b6690a26503
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-12-14 16:35:32 +1100

    Data volume container back/restore scripts

--- scripts/docker_data_volume_container_backup.sh
+++ scripts/docker_data_volume_container_backup.sh
@@ -3,8 +3,10 @@
 # Backup data from data volume container into a tar.gz file in the current directory.
 # Usage: docker_data_volume_container_backup.sh CONTAINER_NAME VOLUME_PATH
 
-print_info(){ printf "$(tput setaf 3)%b$(tput sgr0)\n" "$1" >&2; }
+# Fancy prints
+print_normal (){ printf "%b\n" "$1" >&2; }
 print_error  (){ printf "$(tput setaf 1)[fig-tree] %b$(tput sgr0)\n" "$1" >&2; }
+print_info   (){ printf "$(tput setaf 3)[fig-tree] %b$(tput sgr0)\n" "$1" >&2; }
 print_success(){ printf "$(tput setaf 2)[fig-tree] %b$(tput sgr0)\n" "$1" >&2; }
 
 # Abort in the case of an error
@@ -21,5 +23,8 @@ VOLUME_PATH=$2
 docker run --rm --volumes-from $CONTAINER_NAME -v $(pwd):/backup ubuntu tar czvf /backup/$CONTAINER_NAME.tar.gz $VOLUME_PATH
 handle_error $? "archiving data volume container"
 
+print_normal
 print_success "Data is archived into $CONTAINER_NAME.tar.gz"
+
+exit 0
 
--- scripts/docker_data_volume_container_restore.sh
+++ scripts/docker_data_volume_container_restore.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Restore data from tar.gz into a data volume container
+# Restore data from tar.gz into a data volume container.
 # Usage: docker_data_volume_container_backup.sh CONTAINER_NAME VOLUME_PATH
 
-print_info(){ printf "$(tput setaf 3)%b$(tput sgr0)\n" "$1" >&2; }
+# Fancy prints
+print_normal (){ printf "%b\n" "$1" >&2; }
 print_error  (){ printf "$(tput setaf 1)[fig-tree] %b$(tput sgr0)\n" "$1" >&2; }
+print_info   (){ printf "$(tput setaf 3)[fig-tree] %b$(tput sgr0)\n" "$1" >&2; }
 print_success(){ printf "$(tput setaf 2)[fig-tree] %b$(tput sgr0)\n" "$1" >&2; }
 
 # Abort in the case of an error
@@ -27,7 +29,10 @@ else
   docker run --rm --volumes-from $CONTAINER_NAME -v $(pwd):/backup ubuntu tar xvzf /backup/$CONTAINER_NAME.tar.gz
   handle_error $? "restoring archive into data volume container"
 
+  print_normal
   print_success "Archive $CONTAINER_NAME.tar.gz is restored in $CONTAINER_NAME container"
 fi
+
+exit 0
 
 
